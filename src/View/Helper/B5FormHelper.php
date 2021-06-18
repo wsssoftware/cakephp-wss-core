2adbd32a6db9f6374ad1d356854f9dc9907f84ce
Allan Carvalho <allan.m.carvalho@outlook.com>
2021-06-18 16:20:42 -0300

    Base commit

--- src/View/Helper/B5FormHelper.php
+++ src/View/Helper/B5FormHelper.php
@@ -48,7 +48,10 @@ class B5FormHelper extends FormHelper
                 $options['class'][] = 'is-valid';
             }
         }
-
+        if (!empty($options['help']) && is_string($options['help'])) {
+            $options['data-bs-toggle'] = 'tooltip';
+            $options['title'] = $options['help'];
+        }
         $options['class'] = implode(' ', $options['class']);
         return parent::control($fieldName, $options);
     }
@@ -56,7 +59,7 @@ class B5FormHelper extends FormHelper
     /**
      * @inheritDoc
      */
-    public function checkbox(string $fieldName, array $options = [])
+    public function checkbox(string $fieldName, array $options = []): string
     {
         $id = $this->_domId($fieldName);
         $options['label'] = !empty($options['label']) ? $options['label'] : Inflector::humanize(Inflector::underscore($fieldName));
@@ -64,7 +67,12 @@ class B5FormHelper extends FormHelper
         $options['class'] = !empty($options['class']) ? explode(' ', $options['class']) : [];
         $options['class'][] = 'form-check-input';
         $checkbox = parent::checkbox($fieldName, $options);
-        $label = $this->templater()->format('bs5CheckboxLabel', ['for' => $id, 'content' => $options['label']]);
+        $labelString = $options['label'];
+        if (!empty($options['help']) && is_string($options['help'])) {
+            $helpTitle = $options['help'];
+            $labelString .= " <i class='fa-duotone fa-circle-question text-info-600' data-bs-toggle='tooltip' title='$helpTitle'></i>";
+        }
+        $label = $this->templater()->format('bs5CheckboxLabel', ['for' => $id, 'content' => $labelString]);
         $error = $this->isFieldError($fieldName) ? $this->error($fieldName) : '';
         $templateName = $this->isFieldError($fieldName) ?  'bs5CheckboxWrapperError' : 'bs5CheckboxWrapper';
 
@@ -73,6 +81,56 @@ class B5FormHelper extends FormHelper
             'label' => $label,
             'error' => $error,
         ]);
+    }
 
+    /**
+     * @param string $title
+     * @param array $options
+     * @return string
+     */
+    public function submitButton(string $title, array $options = []): string
+    {
+        $class = !empty($options['class']) ? explode(' ', $options['class']) : [];
+        $class[] = 'btn';
+        $class[] = 'd-block';
+        $class[] = 'fw-500';
+        $options += [
+            'disableOnSubmit' => true,
+            'type' => 'submit',
+            'btn-type' => 'primary',
+            'btn-size' => 'lg',
+            'margin-bottom' => 3,
+            'full-width' => true,
+            'fa' => 'fa-duotone fa-floppy-disk',
+            'data-loading-text' => __('Salvando...'),
+            'data-loading-fa' => null,
+            'data-loading-fa-animation' => 'fa-flip',
+        ];
+        if ($options['disableOnSubmit'] !== false) {
+            $options['onclick'] = 'Toolkit.b5form.disableSubmit(this);';
+        }
+        if (empty($options['data-loading-fa']) && !empty($options['fa'])) {
+            $options['data-loading-fa'] = $options['fa'];
+        }
+        $class[] = "btn-{$options['btn-type']}";
+        $class[] = "btn-{$options['btn-size']}";
+        $class[] = "mb-{$options['margin-bottom']}";
+        if ($options['full-width'] === true) {
+            $class[] = 'w-100';
+        }
+        $options['escapeTitle'] = false;
+        if (!empty($options['fa']) && $options['fa'] !== false) {
+            $title = "<i class='{$options['fa']}'></i> " . $title;
+        }
+        unset($options['disableOnSubmit']);
+        unset($options['btn-type']);
+        unset($options['btn-size']);
+        unset($options['margin-bottom']);
+        unset($options['full-width']);
+        unset($options['fa']);
+
+        $title = "<span>$title</span>";
+        $options['class'] = implode(' ', $class);
+        return parent::button($title, $options);
     }
 }
---
+++ webroot/js/toolkit.js
@@ -0,0 +1,143 @@
+"use strict";
+
+$(document).ready(function () {
+    Toolkit.init();
+});
+
+let Toolkit = {
+    table: null,
+    b5form: null,
+    init: function () {
+        this.table = ToolkitTable;
+        this.b5form = B5Form;
+        this.table.init();
+    },
+};
+
+let B5Form = {
+    disableSubmit: function (element) {
+        let button = $(element);
+        let form = button.parent('form');
+        $(document).on('submit', form, function (e) {
+            let loadingFa = button.data('loading-fa') === undefined ? 'fa-duotone fa-floppy-disk' : button.data('loading-fa');
+            let loadingFaAnimation = button.data('loading-fa-animation') === undefined ? 'fa-spin' : button.data('loading-fa-animation');
+            let loadingText = button.data('loading-text') === undefined ? 'carregando' : button.data('loading-text');
+            let loadingHtml = '<span><i class="' + loadingFa + ' ' + loadingFaAnimation + '"></i> ' + loadingText + '</span>';
+
+            button.addClass('disabled');
+            button.attr('data-html-original', button.html());
+            button.children().fadeOut(300, function () {
+                button.html(loadingHtml);
+                button.children().fadeIn(300);
+            });
+        });
+    },
+    enableSubmit: function (element) {
+        let button = $(element);
+        button.removeClass('disabled');
+        button.children().fadeOut(300, function () {
+            button.html(button.data('html-original'));
+            button.children().fadeIn(300);
+            button.removeAttr('data-html-original');
+        });
+    }
+};
+
+let ToolkitTable = {
+    focusOnSuccess: undefined,
+    init: function () {
+        this.setListeners();
+    },
+    setListeners: function () {
+        $(document).on('click', '.system-table-container a.table-filter-link', function (event) {
+            event.preventDefault();
+            let link = $(this);
+            let url = link.attr('href');
+            history.pushState({}, null, url);
+            this.onUrlChange();
+        });
+        let localOnSearch = this.onSearch(700);
+        $(document).on('keyup', '.table-search-input', function (event) {
+            let notAllowedKeys = [9, 13, 16, 17, 18, 19, 20, 27, 33, 34, 35, 36, 37, 38, 39, 40, 45, 91, 92, 93];
+            console.log('pressed: ' + event.which);
+            if (notAllowedKeys.indexOf(event.which) !== -1) {
+                return false;
+            }
+            this.focusOnSuccess = '#' + $(this).attr('id');
+            localOnSearch();
+        });
+    },
+    onUrlChange: function () {
+        $.ajax({
+            url: location.href,
+            dataType: 'json',
+            headers: {
+                tableUpdate: true,
+            },
+            beforeSend: function( xhr ) {
+                $('.system-table-container .table-responsive').each(function () {
+                    $(this).addClass('table-loading');
+                });
+            },
+            success: function (data, textStatus, jqXHR) {
+                if (data.tables !== undefined) {
+                    $.each(data.tables, function (i, item) {
+                        let table = $('#' + (item.id));
+                        if (table.length) {
+                            table.html(item.body);
+                        }
+                    });
+                }
+                $('.system-table-container .table-responsive').each(function () {
+                    $(this).removeClass('table-loading');
+                });
+                if (this.focusOnSuccess !== undefined) {
+                    $(this.focusOnSuccess).focus();
+                    let inputVal = $(this.focusOnSuccess).val();
+                    $(this.focusOnSuccess).val('').val(inputVal);
+                    this.focusOnSuccess = undefined;
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $('.system-table-container .table-responsive').each(function () {
+                    let element = $(this);
+                    element.removeClass('table-loading');
+                    window.setTimeout(function () {
+                        element.addClass('table-loading-error');
+                    }, 500);
+                });
+            }
+        });
+    },
+    onSearch: function (delay) {
+        let timer = null;
+        return function () {
+            clearTimeout(timer);
+            timer = window.setTimeout(function () {
+                let query = location.search.substring(1);
+                if (query === '' || query === undefined) {
+                    query = JSON.parse('{}');
+                } else {
+                    query = JSON.parse('{"' + decodeURI(query).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+                }
+                $('.table-search-input').each(function () {
+                    let value = $(this).val();
+                    let scope = $(this).data('scope');
+                    if (value === '' || value === undefined) {
+                        delete query[scope + '[query]'];
+                    } else {
+                        query[scope + '[query]'] = value;
+                        delete query[scope + '[page]'];
+                    }
+                });
+                query = $.param(query);
+                let url = window.location.origin + window.location.pathname;
+                if (query !== '' && query !== undefined) {
+                    url += '?' + query;
+                }
+                history.pushState({}, null, url);
+                this.onUrlChange();
+            }, delay || 500);
+        };
+    }
+};
